6973099d9cef6fb7b08937fe5f489c8b92412732
HossainRobin <hellorobinhossain@gmail.com>
2024-06-09 10:15:43 +0600

    flatlist

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,27 @@
-import { View, StyleSheet, ActivityIndicator, Button } from "react-native";
+import {
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  Button,
+  Text,
+  FlatList,
+} from "react-native";
 
 const HomeScreen = () => {
+  const user = [
+    {
+      id: 1,
+      name: "robin",
+    },
+    {
+      id: 2,
+      name: "robin",
+    },
+    {
+      id: 3,
+      name: "robin",
+    },
+  ];
   return (
     <View style={{ marginTop: 50 }}>
       <ActivityIndicator color={"green"} size={"large"} />
@@ -9,6 +30,24 @@ const HomeScreen = () => {
         color={"green"}
         onPress={() => alert("Button clicked")}
         // disabled
+      />
+      <FlatList
+        data={user}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => {
+          return (
+            <View
+              style={{
+                backgroundColor: "#ddd",
+                padding: 15,
+                margin: 10,
+                borderRadius: 5,
+              }}
+            >
+              <Text style={{ fontSize: 22 }}>{item.name}</Text>
+            </View>
+          );
+        }}
       />
     </View>
   );
